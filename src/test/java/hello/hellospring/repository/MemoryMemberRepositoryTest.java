9c245868ae20db7a996c86210196b4654ff0f62a
KJ-HAN1:git config --global user.name KJ-HAN1git config --global user.name KJ-HAN1 <hkjin@kakao.com>
2024-02-22 02:54:14 +0900

    Member_project_Test.java

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,65 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.expression.spel.ast.NullLiteral;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+//    Test메서드가 끝날때마다 동작을 하게끔 하는 콜백 메서드
+//    클레스 단위로 Test를 진행할 경우 순서를 우리가 정할 수 없어 실행서순이 바뀌어 오류 발생 가능
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+//        System.out.println("result = " + (result == member));
+
+//        Assertions.assertEquals(member, result);
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
