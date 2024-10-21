05dfe8d85b22a35b4351815476944ec4e013c0a6
KJ-HAN1:git config --global user.name KJ-HAN1git config --global user.name KJ-HAN1 <hkjin@kakao.com>
2024-10-22 06:23:36 +0900

    Spring Bean setting

---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    //Java code를 통해 스프링빈에 직접 등록하는 방법
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
